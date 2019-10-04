dc57bd4fd98dae8251f759e1e6bf4ca19eb3daa5
MathsionYang <1114809057@qq.com>
2019-10-04 19:11:35 +0800

    Initial commit

---
+++ WordAddImg/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WordAddImg
+{
+    partial class WordAddImg
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WordAddImg));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label_FileEx = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bt_close = new System.Windows.Forms.Button();
+            this.bt_Satrt = new System.Windows.Forms.Button();
+            this.cb_FileEx = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bt_browser = new System.Windows.Forms.Button();
+            this.tb_FilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tb_FileName = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label_FileEx);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.bt_close);
+            this.groupBox1.Controls.Add(this.bt_Satrt);
+            this.groupBox1.Controls.Add(this.cb_FileEx);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.bt_browser);
+            this.groupBox1.Controls.Add(this.tb_FilePath);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(566, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(309, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "]";
+            // 
+            // label_FileEx
+            // 
+            this.label_FileEx.AutoSize = true;
+            this.label_FileEx.Location = new System.Drawing.Point(262, 56);
+            this.label_FileEx.Name = "label_FileEx";
+            this.label_FileEx.Size = new System.Drawing.Size(0, 12);
+            this.label_FileEx.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "[";
+            // 
+            // bt_close
+            // 
+            this.bt_close.Image = ((System.Drawing.Image)(resources.GetObject("bt_close.Image")));
+            this.bt_close.Location = new System.Drawing.Point(470, 68);
+            this.bt_close.Name = "bt_close";
+            this.bt_close.Size = new System.Drawing.Size(37, 26);
+            this.bt_close.TabIndex = 6;
+            this.bt_close.UseVisualStyleBackColor = true;
+            this.bt_close.Click += new System.EventHandler(this.bt_close_Click);
+            // 
+            // bt_Satrt
+            // 
+            this.bt_Satrt.Image = ((System.Drawing.Image)(resources.GetObject("bt_Satrt.Image")));
+            this.bt_Satrt.Location = new System.Drawing.Point(419, 68);
+            this.bt_Satrt.Name = "bt_Satrt";
+            this.bt_Satrt.Size = new System.Drawing.Size(34, 26);
+            this.bt_Satrt.TabIndex = 5;
+            this.bt_Satrt.UseVisualStyleBackColor = true;
+            this.bt_Satrt.Click += new System.EventHandler(this.bt_Satrt_Click);
+            // 
+            // cb_FileEx
+            // 
+            this.cb_FileEx.FormattingEnabled = true;
+            this.cb_FileEx.Items.AddRange(new object[] {
+            "*.JPG",
+            "*.PNG",
+            "*.TIF"});
+            this.cb_FileEx.Location = new System.Drawing.Point(90, 53);
+            this.cb_FileEx.Name = "cb_FileEx";
+            this.cb_FileEx.Size = new System.Drawing.Size(121, 20);
+            this.cb_FileEx.TabIndex = 4;
+            this.cb_FileEx.SelectedIndexChanged += new System.EventHandler(this.cb_FileEx_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("隶书", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "图片类型：";
+            // 
+            // bt_browser
+            // 
+            this.bt_browser.Image = ((System.Drawing.Image)(resources.GetObject("bt_browser.Image")));
+            this.bt_browser.Location = new System.Drawing.Point(483, 12);
+            this.bt_browser.Name = "bt_browser";
+            this.bt_browser.Size = new System.Drawing.Size(38, 31);
+            this.bt_browser.TabIndex = 2;
+            this.bt_browser.UseVisualStyleBackColor = true;
+            this.bt_browser.Click += new System.EventHandler(this.bt_browser_Click);
+            // 
+            // tb_FilePath
+            // 
+            this.tb_FilePath.Location = new System.Drawing.Point(90, 18);
+            this.tb_FilePath.Name = "tb_FilePath";
+            this.tb_FilePath.Size = new System.Drawing.Size(387, 21);
+            this.tb_FilePath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("隶书", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件夹路径：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tb_FileName);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox2.Font = new System.Drawing.Font("隶书", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(6, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(300, 269);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "文件名";
+            // 
+            // tb_FileName
+            // 
+            this.tb_FileName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_FileName.Location = new System.Drawing.Point(3, 17);
+            this.tb_FileName.Multiline = true;
+            this.tb_FileName.Name = "tb_FileName";
+            this.tb_FileName.Size = new System.Drawing.Size(294, 249);
+            this.tb_FileName.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox5);
+            this.groupBox3.Controls.Add(this.groupBox2);
+            this.groupBox3.Location = new System.Drawing.Point(12, 133);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(566, 302);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox5.BackgroundImage")));
+            this.groupBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBox5.Location = new System.Drawing.Point(312, 20);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(248, 266);
+            this.groupBox5.TabIndex = 2;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "   ";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Location = new System.Drawing.Point(12, 442);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(562, 32);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("隶书", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(137, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(281, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "湖北大学资源环境学院地图学与地理信息系统--杨洋";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // WordAddImg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 486);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("隶书", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WordAddImg";
+            this.Text = "Word图片添加";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bt_browser;
+        private System.Windows.Forms.TextBox tb_FilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_FileEx;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bt_Satrt;
+        private System.Windows.Forms.Button bt_close;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tb_FileName;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label_FileEx;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WordAddImg/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MSWord = Microsoft.Office.Interop.Word;
+using System.IO;
+using System.Reflection;
+
+namespace WordAddImg
+{
+    public partial class WordAddImg : Form
+    {
+        public WordAddImg()
+        {
+            InitializeComponent();
+        }
+        //选择图片所在文件夹
+        private void bt_browser_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            //对话框的名字
+            dialog.Description = "Please choose the path ";
+            string foldPath = "";
+            //点击对话框的确定之后执行
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                foldPath = dialog.SelectedPath;
+            }
+            tb_FilePath.Text = foldPath;
+        }
+        //开始执行
+        private void bt_Satrt_Click(object sender, EventArgs e)
+        {
+            tb_FileName.Text = "开始执行，请稍候！"+ Environment.NewLine;
+            
+            object path;                              //文件路径变量
+            MSWord.Application wordApp;                   //Word应用程序变量 
+            MSWord.Document wordDoc;                  //Word文档变量
+
+            path = Environment.CurrentDirectory + "\\MyWord_Print.doc";
+            wordApp = new MSWord.ApplicationClass(); //初始化
+            wordApp.Visible = true;//使文档可见
+            //如果已存在，则删除
+            if (File.Exists((string)path))
+            {
+                File.Delete((string)path);
+            }
+            //由于使用的是COM库，因此有许多变量需要用Missing.Value代替
+            Object Nothing = Missing.Value;
+            wordDoc = wordApp.Documents.Add(ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+            #region 页面设置、页眉图片和文字设置，最后跳出页眉设置
+            //页面设置
+            wordDoc.PageSetup.PaperSize = MSWord.WdPaperSize.wdPaperA4;//设置纸张样式为A4纸
+            wordDoc.PageSetup.Orientation = MSWord.WdOrientation.wdOrientPortrait;//排列方式为垂直方向
+            wordDoc.PageSetup.TopMargin = 57.0f;
+            wordDoc.PageSetup.BottomMargin = 57.0f;
+            wordDoc.PageSetup.LeftMargin = 57.0f;
+            wordDoc.PageSetup.RightMargin = 57.0f;
+            wordDoc.PageSetup.HeaderDistance = 30.0f;//页眉位置
+            #endregion
+            #region 页码设置并添加页码
+            //为当前页添加页码
+            MSWord.PageNumbers pns = wordApp.Selection.Sections[1].Headers[MSWord.WdHeaderFooterIndex.wdHeaderFooterEvenPages].PageNumbers;//获取当前页的号码
+            pns.NumberStyle = MSWord.WdPageNumberStyle.wdPageNumberStyleNumberInDash;//设置页码的风格，是Dash形还是圆形的
+            pns.HeadingLevelForChapter = 0;
+            pns.IncludeChapterNumber = false;
+            pns.RestartNumberingAtSection = false;
+            pns.StartingNumber = 0; //开始页页码？
+            object pagenmbetal = MSWord.WdPageNumberAlignment.wdAlignPageNumberCenter;//将号码设置在中间
+            object first = true;
+            wordApp.Selection.Sections[1].Footers[MSWord.WdHeaderFooterIndex.wdHeaderFooterEvenPages].PageNumbers.Add(ref pagenmbetal, ref first);
+            #endregion
+            object unite = MSWord.WdUnits.wdStory;
+            #region 添加表格、填充数据、设置表格行列宽高、合并单元格、添加表头斜线、给单元格添加图片
+            wordDoc.Content.InsertAfter("\n");//这一句与下一句的顺序不能颠倒，原因还没搞透
+            wordApp.Selection.EndKey(ref unite, ref Nothing); //将光标移动到文档末尾
+            wordApp.Selection.ParagraphFormat.Alignment = MSWord.WdParagraphAlignment.wdAlignParagraphLeft;
+
+            //遍历文件夹下所有jpg
+            String path_img = tb_FilePath.Text;
+            String FileEx = label_FileEx.Text;
+            var files = Directory.GetFiles(path_img, FileEx);
+            List<string> testList = new List<string>();
+            foreach (var file in files)
+                testList.Add(file);
+            //设置表格的行数和列数
+            int testList_Count = testList.Count;
+            int tableRow = testList_Count;
+            int tableColumn = 2;
+
+            //定义一个Word中的表格对象
+            MSWord.Table table = wordDoc.Tables.Add(wordApp.Selection.Range,
+            tableRow, tableColumn, ref Nothing, ref Nothing);
+            //默认创建的表格没有边框，这里修改其属性，使得创建的表格带有边框 
+            table.Borders.Enable = 1;//这个值可以设置得很大，例如5、13等等
+            //添加行
+            table.Rows.Add(ref Nothing);
+            table.Rows[tableRow + 1].Height = 35;//设置新增加的这行表格的高度
+            //向新添加的行的单元格中添加图片
+            object LinkToFile = false;
+            object SaveWithDocument = true;
+            object Anchor;
+            for (int i = 0; i < testList_Count; i++)
+            {
+                int img_num = i + 1;
+                tb_FileName.Text += "正在处理第"+ img_num + "张图片。"+ Environment.NewLine;
+                string FileName = testList[i];
+                //获取没有扩展名的文件名存入List::FileNameList
+                string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(FileName);// 没有
+                if (i % 2 == 0)
+                {
+                    Anchor = table.Cell(i + 1, 1).Range;//选中要添加图片的单元格
+                    wordDoc.Application.ActiveDocument.InlineShapes.AddPicture(FileName, ref LinkToFile, ref SaveWithDocument, ref Anchor);
+                    table.Cell(i+2, 1).Range.Text = fileNameWithoutExtension;//填充每列的标题
+                }
+                else
+                {
+                    Anchor = table.Cell(i, 2).Range;//选中要添加图片的单元格
+                    wordDoc.Application.ActiveDocument.InlineShapes.AddPicture(FileName, ref LinkToFile, ref SaveWithDocument, ref Anchor);
+                    table.Cell(i+1, 2).Range.Text = fileNameWithoutExtension;//填充每列的标题
+                }
+            }
+            
+
+            //设置table样式
+            table.Rows.HeightRule = MSWord.WdRowHeightRule.wdRowHeightAtLeast;//高度规则是：行高有最低值下限？
+            table.Rows.Height = wordApp.CentimetersToPoints(float.Parse("0.8"));// 
+
+            table.Range.Font.Size = 10.5F;
+            table.Range.Font.Bold = 0;
+
+            table.Range.ParagraphFormat.Alignment = MSWord.WdParagraphAlignment.wdAlignParagraphCenter;//表格文本居中
+            table.Range.Cells.VerticalAlignment = MSWord.WdCellVerticalAlignment.wdCellAlignVerticalBottom;//文本垂直贴到底部
+            //设置table边框样式
+            table.Borders.OutsideLineStyle = MSWord.WdLineStyle.wdLineStyleDouble;//表格外框是双线
+            table.Borders.InsideLineStyle = MSWord.WdLineStyle.wdLineStyleSingle;//表格内框是单线
+
+            table.Rows[1].Range.Font.Bold = 1;//加粗
+            table.Rows[1].Range.Font.Size = 12F;
+            table.Cell(1, 1).Range.Font.Size = 10.5F;
+            wordApp.Selection.Cells.Height = 30;//所有单元格的高度
+
+            //除第一行外，其他行的行高都设置为20
+            for (int i = 2; i <= tableRow; i++)
+            {
+                table.Rows[i].Height = 20;
+            }
+            table.Columns[1].Width = 50;//将第 1列宽度设置为50
+
+            //将其他列的宽度都设置为75
+            for (int i = 2; i <= tableColumn; i++)
+            {
+                table.Columns[i].Width = 50;
+            }
+            #endregion
+            wordApp.Selection.EndKey(ref unite, ref Nothing); //将光标移动到文档末尾
+            wordDoc.Content.InsertAfter("\n");
+            //WdSaveFormat为Word 2003文档的保存格式
+            object format = MSWord.WdSaveFormat.wdFormatDocument;// office 2007就是wdFormatDocumentDefault
+            //将wordDoc文档对象的内容保存为DOCX文档
+            wordDoc.SaveAs(ref path, ref format, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing, ref Nothing);
+            //关闭wordDoc文档对象
+            //看是不是要打印
+            //wordDoc.PrintOut();
+            wordDoc.Close(ref Nothing, ref Nothing, ref Nothing);
+            //关闭wordApp组件对象
+            wordApp.Quit(ref Nothing, ref Nothing, ref Nothing);
+            tb_FileName.Text += "全部执行完毕";
+        }
+
+        private void cb_FileEx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.label_FileEx.Text = cb_FileEx.Text;
+        }
+
+        private void bt_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
